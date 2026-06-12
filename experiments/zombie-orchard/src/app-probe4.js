1a82c696d057d6dde9b3fb4d5b88aefd8e01cba4
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-11 23:02:14 -0400

    Neutralize temporary probe

--- experiments/zombie-orchard/src/app-probe4.js
+++ experiments/zombie-orchard/src/app-probe4.js
@@ -1,9 +1,1 @@
-let last = performance.now();
-function frame(now, game, input, view, drawHud) {
-  const dt = Math.min(1 / 20, (now - last) / 1000 || 1 / 60);
-  last = now;
-  const snap = game.update(dt, input.read());
-  view.render(snap);
-  drawHud(snap);
-  requestAnimationFrame((next) => frame(next, game, input, view, drawHud));
-}
+removed
