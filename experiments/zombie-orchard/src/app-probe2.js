2cb699bd6726042c6ad4f21c200c82c3778e4902
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-11 23:01:09 -0400

    Remove temporary probe

--- experiments/zombie-orchard/src/app-probe2.js
+++
@@ -1,4 +0,0 @@
-const $ = (id) => document.querySelector(id);
-const canvas = $("#orchard-canvas");
-const ids = ["round", "score", "apples", "weapon", "health", "stamina", "pressure"];
-const hud = Object.fromEntries(ids.map((id) => [id, $(`#${id}`)]));
